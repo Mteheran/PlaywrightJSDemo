5157d10f0f2dececaf4914601d40fcd7891db0f8
Miguel Teheran <miguelteheran@gmail.com>
2023-11-22 21:50:15 -0500

    adding testing api

---
+++ tests/api.spec.js
@@ -0,0 +1,30 @@
+import { expect, test } from '@playwright/test';
+
+// Request context is reused by all tests in the file.
+let apiContext;
+
+test.beforeAll(async ({ playwright }) => {
+  apiContext = await playwright.request.newContext({
+    // All requests we send go to this API endpoint.
+    baseURL: 'https://api-colombia.com/',
+  });
+});
+
+test.afterAll(async ({ }) => {
+    // Dispose all responses.
+    await apiContext.dispose();
+});
+
+test('colombia internet domain', async () => {
+    const infoColombia = await apiContext.get(`/api/v1/Country/Colombia`);
+    expect(infoColombia.ok()).toBeTruthy();
+    const responseBody = await infoColombia.json()
+    expect(responseBody).toHaveProperty("internetDomain", ".co")
+});
+
+test('Regions of Colombia', async () => {
+    const infoColombia = await apiContext.get(`/api/v1/Region`);
+    expect(infoColombia.ok()).toBeTruthy();
+    const responseBody = await infoColombia.json();
+    expect(responseBody).toHaveLength(6);
+});
